3fcfcf07808faafaf70e72e3fc2c6f6b1afd283e
Allister Skiba <a.skiba@kids-interactive.de>
2023-01-20 08:48:08 +0100

    Updates composer.json and ext_emconf.php to allow usage in typo3v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,14 +9,14 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Friends of TYPO3',
     'author_email' => 'friendsof@typo3.org',
     'author_company' => '',
-    'version' => '10.0.0',
+    'version' => '11.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.9.99',
+            'typo3' => '11.0.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
-            'sys_action' => '10.0.0',
+            'sys_action' => '11.0.0',
         ],
     ],
 ];
